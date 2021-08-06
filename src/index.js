83c41856b5df8b09b7e93ac7e4f5c12e91d444a0
coldfear457@gmail.com <coldfear457@gmail.com>
2021-08-06 19:43:19 +0600

    feat: add solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    var str = String(n);
+    arr = str.split('').reverse(),
+        newStr = '';
+
+
+    for (let i = 0; i < arr.length; i++) {
+        newStr += arr[i];
+    }
+    n = parseInt(newStr)
+    return n;
 }
